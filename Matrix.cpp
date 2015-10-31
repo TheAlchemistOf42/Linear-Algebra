c5bfa7e82d4082a5ac484ab0cdb504526bc3b4cf
A Crutcher <alcrutcher14@gmail.com>
2015-10-31 17:53:00 -0500

    Moidification
    Redid some of the  formatting. Added some comments removed namespace std

--- Matrix.cpp
+++ Matrix.cpp
@@ -9,7 +9,6 @@ Description: A matrix object allowing one to do mathematics with matrices.
 */
 
 #include"Matrix.h"
-using namespace std;
 Matrix createIdentity(int R);
 
 /*--------------------------------Constructors--------------------------------*/
@@ -41,9 +40,7 @@ Matrix::Matrix(const Matrix & orig)
 	m_array = new double[orig.m_size];
 	// Check memory, I guess
 	for (int i = 0; i < orig.m_size; i++)
-	{
 		m_array[i] = orig.m_array[i];
-	}
 	m_rows = orig.m_rows;
 	m_columns = orig.m_columns;
 	m_size = orig.m_size;
@@ -112,9 +109,7 @@ void Matrix::rowScalar(int p_row, double p_multiplier)
 	if (p_row <= m_rows && p_row >= 1)
 	{
 		for (int i = (p_row - 1)*m_columns; i < (p_row*m_columns); i++)
-		{
 			m_array[i] *= p_multiplier;
-		}
 	}
 }
 
@@ -167,9 +162,7 @@ void Matrix::setRow(const double p_array[], int p_size, int p_rowNum)
 	if (p_size == m_columns && p_rowNum >= 1 && p_rowNum <= m_rows)
 	{
 		for (int i = m_columns * (p_rowNum - 1); i < p_rowNum*m_columns; i++)
-		{
 			m_array[i] = p_array[i % m_columns];
-		}
 	}
 }
 
@@ -184,9 +177,7 @@ void Matrix::setColumn(const double p_array[], int p_size, int p_colNum)
 	{
 		for (int i = p_colNum - 1; i < m_columns*(m_rows - 1) + p_colNum;
 			 i += m_columns)
-		{
 			m_array[i] = p_array[i / m_columns];
-		}
 	}
 }
 
@@ -213,21 +204,12 @@ Postcondition:  Will return true if the operation will work,
 bool Matrix::checkOperation(const Matrix & other, char oper)
 {
 	// Need to implement this
-	if (oper == '+' || oper == '-')
-		// Addition or subtraction
-	{
+	if (oper == '+' || oper == '-') // Addition or subtraction
 		return (m_columns == other.m_columns && m_rows == other.m_rows);
-	}
-	else if (oper == '*')
-		// Matrix multiplication (Ax=b) where A is host Matrix, x is other
-	{
+	else if (oper == '*') // Matrix multiplication (Ax=b) where A is host Matrix
 		return (m_columns == other.m_rows);
-	}
-	else if (oper == 'a')
-		// Augment Matrix
-	{
+	else if (oper == 'a') // Augment Matrix
 		return (m_rows == other.m_rows);
-	}
 	// Equals (=), Equal to (==) and Not equal to (!=) do not have requirements.
 	// Transpose, inverse, and determinant do not need another Matrix.
 	// Default Assumption
@@ -238,10 +220,8 @@ bool Matrix::checkOperation(const Matrix & other, char oper)
 Precondition: Intialized matrices and both be the same size
 Postcondition: The sum of both matrices will be returned,
 	else the original matrix will be returned.            */
-Matrix Matrix::operator+(const Matrix & p_term)
-{
-	if (m_rows == p_term.m_rows && m_columns == p_term.m_columns)
-	{
+Matrix Matrix::operator+(const Matrix & p_term){
+	if (m_rows == p_term.m_rows && m_columns == p_term.m_columns){
 		double * temp = new double[m_size];
 		// check alloc
 		for (int i = 0; i < m_size; i++)
@@ -258,10 +238,8 @@ Matrix Matrix::operator+(const Matrix & p_term)
 Precondition: Intialized matrices and both be the same size
 Postcondition: The difference of both matrices will be returned,
 	else the original matrix will be returned.                 */
-Matrix Matrix::operator-(const Matrix & p_term)
-{
-	if (m_rows == p_term.m_rows && m_columns == p_term.m_columns)
-	{
+Matrix Matrix::operator-(const Matrix & p_term){
+	if (m_rows == p_term.m_rows && m_columns == p_term.m_columns)	{
 		double * temp = new double[m_size];
 		// check alloc
 		for (int i = 0; i < m_size; i++)
@@ -278,22 +256,18 @@ Matrix Matrix::operator-(const Matrix & p_term)
 Precondition: Intialized matrices and both be the same size
 Postcondition: The difference of both matrices will be returned,
 	else the original matrix will be returned.                 */
-Matrix Matrix::operator*(const Matrix & p_term)
-{
+Matrix Matrix::operator*(const Matrix & p_term){
 	// May be able to make it one for loop
-	if (m_columns == p_term.m_rows)
-	{
+	if (m_columns == p_term.m_rows){
 		int productSize = m_rows * p_term.m_columns;
 		double * productArray = new double[productSize];
 		int row = 0,
 			column = 0;
-		for (int i = 0; i < m_rows * p_term.m_columns; i++)
-		{
+		for (int i = 0; i < m_rows * p_term.m_columns; i++){
 			double sum = 0;
 			row = i / m_columns;
 			column = i % m_columns;
-			for (int k = 0; k < m_columns; k++)
-			{
+			for (int k = 0; k < m_columns; k++){
 				sum += m_array[k + row * m_columns]
 					 * p_term.m_array[k * p_term.m_columns + column];
 			}
@@ -303,15 +277,13 @@ Matrix Matrix::operator*(const Matrix & p_term)
 		delete[] productArray;
 		return product;
 	}
-
 	return *this;
 }
 
 /* Purpose: Sets the calling matrix equal to the term Matrix
 Precondition: Intialized matrices
 Postcondition: The calling matrix will be the same as the term matrix.	  */
-Matrix & Matrix::operator=(const Matrix & p_term)
-{
+Matrix & Matrix::operator=(const Matrix & p_term){
 	delete[] m_array;
 	m_rows = p_term.m_rows;
 	m_columns = p_term.m_columns;
@@ -327,108 +299,77 @@ Matrix & Matrix::operator=(const Matrix & p_term)
 Precondition: The matrix must be invertible
 Postcondition: The inverse of the matrix will be returned,
 	else the matrix itself will be returned.				*/
-Matrix Matrix::inverse()
-{
-	if (m_rows == m_columns)
-	{
+Matrix Matrix::inverse(){
+	if (m_rows == m_columns){
 		double * inverse = new double[m_size];
 		double deter = determinant();
-		if (deter != 0)
-		{
-
-			if (m_rows == 1)
-				// Single element
-			{
+		if (deter != 0){
+			if (m_rows == 1){ // Single element
 				inverse[0] = 1.0 / m_array[0];
-			}
-			else if (m_rows == 2)
-				// 2x2 matrix
-			{
+			} else if (m_rows == 2){ // 2x2 matrix
 				inverse[0] = 1.0 * m_array[3] / deter;
 				inverse[1] = -1.0 * m_array[1] / deter;
 				inverse[2] = -1.0 * m_array[2] / deter;
 				inverse[3] = 1.0 * m_array[0] / deter;
-			}
-			else
-			{
+			} else {
 				// Use row reduce reduction formula, may be rounding issues
 				Matrix invert = createIdentity(m_columns);
 				int column = 0;
-				for (int row = 0; row < m_rows; row++)
-				{
-					if (m_array[row * m_columns + column] != 0)
-					{
+				for (int row = 0; row < m_rows; row++){
+					if (m_array[row * m_columns + column] != 0){
 						// Reduces the front of the row to 0
 						invert.rowScalar(row + 1, 1.0 / m_array[row * m_columns + column]);
 						rowScalar(row + 1, 1.0 / m_array[row * m_columns + column]);
 						// Reduces the rest of the values below to 0
 						for (int i = (row + 1) * m_columns + column; i < m_size;
-							i += m_columns)
-						{
-							if (m_array[i] != 0)
-							{
+							i += m_columns){
+							if (m_array[i] != 0){
 								invert.replacement(i / m_columns + 1, row + 1, -1.0 * m_array[i]);
 								replacement(i / m_columns + 1, row + 1, -1.0 * m_array[i]);
 							}
 						}
 						// Reduces the rest of the values above to 0
-						for (int i = column; i < row * m_columns + column; i += m_columns)
-						{
-							if (m_array[i] != 0)
-							{
+						for (int i = column; i < row * m_columns + column; i += m_columns){
+							if (m_array[i] != 0){
 								invert.replacement(i / m_columns + 1, row + 1, -1.0 * m_array[i]);
 								replacement(i / m_columns + 1, row + 1, -1.0 * m_array[i]);
 							}
 						}
-					}
-					else
-					{
+					} else {
 						// row interchange with ones below
-						double max = 0,
-							min = 0;
-						int maxRow = 0,
-							minRow = 0;
+						double max = 0, min = 0;
+						int maxRow = 0,	minRow = 0;
 						for (int i = (row + 1) * m_columns + column; i < m_size;
-							i += m_columns)
-						{
-							if (m_array[i] < min)
-							{
+							i += m_columns){
+							if (m_array[i] < min){
 								min = m_array[i];
 								minRow = i / m_columns + 1;
-							}
-							else if (m_array[i] > max)
-							{
+							} else if (m_array[i] > max) {
 								max = m_array[i];
 								maxRow = i / m_columns + 1;
 							}
 						}
 						// if max and min are 0 skip to next row, column
 						// Max and min are checked in case there is a negative value
-						if (max != 0)
-						{
+						if (max != 0) {
 							interchange(row + 1, maxRow);
 							invert.interchange(row + 1, maxRow);
 							row--; // Force it to go through the first part of if statement
 							column--;
 						}
-						else if (min != 0)
-						{
+						else if (min != 0){
 							interchange(row + 1, minRow);
 							invert.interchange(row + 1, minRow);
 							row--; // Force it to go through the first part of if statement
 							column--;
-						}
-						else
-						{
-							row--; // Both max and min of ones below are 0
+						}else
+							row--;
+							// Both max and min of ones below are 0
 							// Stay in the same row, move onto the next column
-						}
 					}
 					column++;
 					if (column >= m_columns)
-					{
 						return invert;
-					}
 				}
 			}
 			Matrix invert(inverse, m_rows, m_columns);
@@ -444,24 +385,15 @@ Matrix Matrix::inverse()
 Precondition: Intialized matrices with the same number of rows
 Postcondition: The calling matrix will add term matrix, creating
 	an augmented matrix.   										     */
-void Matrix::augment(const Matrix & p_term)
-{
-	if (m_rows == p_term.m_rows)
-	{
+void Matrix::augment(const Matrix & p_term){
+	if (m_rows == p_term.m_rows){
 		double * temp = new double[m_size + p_term.m_size];
 		int c = 0;
-		for (int i = 0; i < m_rows; i++)
-		{
-			for (int m1 = 0; m1 < m_columns; m1++)
-			{
+		for (int i = 0; i < m_rows; i++){
+			for (int m1 = 0; m1 < m_columns; m1++, c++)
 				temp[c] = m_array[i*m_columns + m1];
-				c++;
-			}
-			for (int m2 = 0; m2 < p_term.m_columns; m2++)
-			{
+			for (int m2 = 0; m2 < p_term.m_columns; m2++, c++)
 				temp[c] = p_term.m_array[i*p_term.m_columns + m2];
-				c++;
-			}
 		}
 		m_columns += p_term.m_columns;
 		m_size = m_columns * m_rows;
@@ -473,21 +405,16 @@ void Matrix::augment(const Matrix & p_term)
 /* Purpose: Transpose the matrix
 Precondition: Intialized matrix
 Postcondition: The rows of the matrix will become its columns   */
-void Matrix::transpose()
-{
+void Matrix::transpose(){
 	// see about reducing it to one for loop
-	if (m_rows != 1 && m_columns != 1)
-	{
+	if (m_rows != 1 && m_columns != 1){
 		double * temp = new double[m_size];
 		// Check memory, I guess
 		// r designates row#-1 in original, col#-1 in transpose
-		for (int r = 0; r < m_rows; r++)
-		{
+		for (int r = 0; r < m_rows; r++){
 			// i designates col#-1 in original, row#-1 in transpose
 			for (int i = 0; i < m_columns; i++)
-			{
 				temp[i*m_rows + r] = m_array[r*m_columns + i];
-			}
 		}
 		delete[] m_array;
 		m_array = temp;
@@ -501,88 +428,61 @@ void Matrix::transpose()
 Precondition: Intialized matrix
 Postcondition: The matrix will now be in RREF,
 	though note, there may be rounding errors.			*/
-void Matrix::rref()
-{
+void Matrix::rref(){
 	// m_columns, m_rows
 	int column = 0;
-	for (int row = 0; row < m_rows; row++)
-	{
-		if (m_array[row * m_columns + column] != 0)
-		{
+	for (int row = 0; row < m_rows; row++){
+		if (m_array[row * m_columns + column] != 0){
 			// Reduces the front of the row to 0
 			rowScalar(row + 1, 1.0 / m_array[row * m_columns + column]);
 			// Reduces the rest of the values below to 0
 			for (int i = (row + 1) * m_columns + column; i < m_size;
-				i += m_columns)
-			{
+				i += m_columns){
 				if (m_array[i] != 0)
-				{
 					replacement(i / m_columns + 1, row + 1, -1.0 * m_array[i]);
-				}
 			}
 			// Reduces the rest of the values above to 0
-			for (int i = column; i < row * m_columns + column; i += m_columns)
-			{
+			for (int i = column; i < row * m_columns + column; i += m_columns){
 				if (m_array[i] != 0)
-				{
 					replacement(i / m_columns + 1, row + 1, -1.0 * m_array[i]);
-				}
 			}
-		}
-		else
-		{
+		} else {
 			// row interchange with ones below
-			double max = 0,
-				min = 0;
-			int maxRow = 0,
-				minRow = 0;
+			double max = 0,	min = 0;
+			int maxRow = 0,	minRow = 0;
 			for (int i = (row + 1) * m_columns + column; i < m_size;
-				i += m_columns)
-			{
-				if (m_array[i] < min)
-				{
+				i += m_columns)	{
+				if (m_array[i] < min){
 					min = m_array[i];
 					minRow = i / m_columns + 1;
-				}
-				else if (m_array[i] > max)
-				{
+				} else if (m_array[i] > max) {
 					max = m_array[i];
 					maxRow = i / m_columns + 1;
 				}
 			}
 			// if max and min are 0 skip to next row, column
 			// Max and min are checked in case there is a negative value
-			if (max != 0)
-			{
+			if (max != 0) {
 				interchange(row + 1, maxRow);
 				row--; // Force it to go through the first part of if statement
 				column--;
-			}
-			else if (min != 0)
-			{
+			} else if (min != 0) {
 				interchange(row + 1, minRow);
 				row--; // Force it to go through the first part of if statement
 				column--;
-			}
-			else
-			{
+			} else {
 				row--; // Both max and min of ones below are 0
 				// Stay in the same row, move onto the next column
 			}
 		}
 		column++;
 		if (column >= m_columns)
-		{
 			break;
-		}
 	}
-	for (int i = 0; i < m_size; i++)
-	{
+	for (int i = 0; i < m_size; i++){
+		// Fixes problems where 0's sometimes have negative signs after rref
 		if (m_array[i] == 0)
-			// Fixes problems where 0's sometimes have negative signs after rref
-		{
 			m_array[i] = abs(m_array[i]);
-		}
 	}
 }
 
@@ -590,14 +490,10 @@ void Matrix::rref()
 Precondition: Intialized matrices
 Postcondition: Return true if they are the same, else
 	return false                                      */
-bool Matrix::operator==(const Matrix & p_term)
-{
+bool Matrix::operator==(const Matrix & p_term){
 	if (m_rows != p_term.m_rows || m_columns != p_term.m_columns)
-	{
 		return false;
-	}
-	for (int i = 0; i < m_size; i++)
-	{
+	for (int i = 0; i < m_size; i++){
 		if (m_array[i] != p_term.m_array[i])
 			return false;
 	}
@@ -608,14 +504,10 @@ bool Matrix::operator==(const Matrix & p_term)
 Precondition: Intialized matrices
 Postcondition: Return true if they are not the same, else
 	return true                                           */
-bool Matrix::operator!=(const Matrix & p_term)
-{
+bool Matrix::operator!=(const Matrix & p_term){
 	if (m_rows != p_term.m_rows || m_columns != p_term.m_columns)
-	{
 		return true;
-	}
-	for (int i = 0; i < m_size; i++)
-	{
+	for (int i = 0; i < m_size; i++){
 		if (m_array[i] != p_term.m_array[i])
 			return true;
 	}
@@ -626,47 +518,34 @@ bool Matrix::operator!=(const Matrix & p_term)
 Precondition: It must be an n*n matrix
 Postcondition: It will return the determinant if it is n*n
 	otherwise return 0 */
-double Matrix::determinant()
-{
+double Matrix::determinant(){
 	// This function is recursive
-	if (m_rows == m_columns)
-	{
+	if (m_rows == m_columns)	{
 		// 1x1 trivial case
 		if (m_rows == 1)
-		{
 			return m_array[0];
-		}
 		// 2x2 down - up, ad - cb
 		else if (m_rows == 2)
-		{
 			return m_array[0] * m_array[3] - m_array[2] * m_array[1];
-		}
 		// 3x3 rule, aei + bfg + cdh - gec - hfa - idb
 		else if (m_rows == 3)
-		{
 			return (m_array[0] * m_array[4] * m_array[8]
 				  + m_array[1] * m_array[5] * m_array[6]
 				  + m_array[2] * m_array[3] * m_array[7]
 				  - m_array[6] * m_array[4] * m_array[2]
 				  - m_array[7] * m_array[5] * m_array[0]
 				  - m_array[8] * m_array[3] * m_array[1]);
-		}
 		// n*n, cofactor expansion
-		else
-		{
+		else {
 			// This will be a bit inefficient, need to see about improving
 			double deter = 0;
 			Matrix m;
 			double * tempArray = new double[(m_columns - 1) * (m_rows - 1)];
-			for (int i = 0; i < m_columns; i++)
-			{
-				if (m_array[i] != 0)
-				{
+			for (int i = 0; i < m_columns; i++)	{
+				if (m_array[i] != 0){
 					int newM = 0;
-					for (int orig = m_columns; orig < m_size; orig++)
-					{
-						if (orig % m_columns != i % m_columns)
-						{
+					for (int orig = m_columns; orig < m_size; orig++){
+						if (orig % m_columns != i % m_columns){
 							tempArray[newM] = m_array[orig];
 							newM++;
 						}
@@ -674,6 +553,7 @@ double Matrix::determinant()
 					m.resetMatrix(tempArray, m_columns - 1, m_rows - 1);
 					deter += m_array[i] * m.determinant()
 					  	   * pow(-1, i / m_columns + i % m_columns);
+					// deter += m_... * ((i / m_columns + i % m_columns) % 2 == 0 ? 1 : -1);
 				}
 			}
 			delete[] tempArray;
@@ -690,16 +570,14 @@ Precondition: Intialized matrix and valid output stream
 Postcondition: The matrix will be outputted to the output stream,
 	so it is in rows and columns like a matrix is typically drawn.
 	If uninitialized, nothing is outputted.                        */
-void Matrix::displayMatrix(ostream & out)
-{
+void Matrix::displayMatrix(std::ostream & out){
 	out << left;
 	if (m_array == 0 || m_size == 0) return;
-	for (int i = 0; i < m_size; i++)
-	{
+	for (int i = 0; i < m_size; i++){
 		if (i % m_columns != 0 || i == 0)
-			out << setw(3) << m_array[i] << ' ';
+			out << std::setw(3) << m_array[i] << ' ';
 		else
-			out << '\n' << setw(3) << m_array[i] << ' ';
+			out << '\n' << std::setw(3) << m_array[i] << ' ';
 	}
 	out << '\n';
 }
@@ -710,29 +588,21 @@ void Matrix::displayMatrix(ostream & out)
 Precondition: R is greater then or equal to 1
 Postcondition: The matrix return will be an Identity matrix,
 R does not meet Precondition, then an empty matrix will be returned*/
-Matrix createIdentity(int R)
-{
-	if (R >= 1)
-	{
+Matrix createIdentity(int R){
+	if (R >= 1)	{
 		int insert1 = 0;
 		double * array = new double[R*R];
-		for (int k = 0; k < R*R; k++)
-		{
-			if (k == insert1)
-			{
+		for (int k = 0; k < R*R; k++){
+			if (k == insert1){
 				array[k] = 1.0;
 				insert1 += R + 1;
-			}
-			else
-			{
+			} else
 				array[k] = 0.0;
-			}
 		}
 		Matrix identity(array, R, R);
 		delete[] array;
 		return identity;
 	}
-
 	return Matrix();
 }
 
